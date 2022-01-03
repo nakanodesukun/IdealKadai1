5d0194466b27714458fcf872db9128f501a96d92
nakanodesukun <94818124+nakanodesukun@users.noreply.github.com>
2022-01-03 23:40:26 +0900

    .mapと.reduceを使ってKadai1を改善しました。mapとreduceを使いこなせるようになりたい

--- IdealKadai1/ViewController.swift
+++ IdealKadai1/ViewController.swift
@@ -17,18 +17,30 @@ class ViewController: UIViewController {
     @IBOutlet private weak var resultLabel: UILabel!
 
 
-    
     @IBAction func addButton(_ sender: Any) {
-a()
+//        let text1 = Int(textField1.text ?? "") ?? 0
+//        let text2 = Int(textField2.text ?? "") ?? 0
+//        let text3 = Int(textField3.text ?? "") ?? 0
+//        let text4 = Int(textField4.text ?? "") ?? 0
+//        let text5 = Int(textField5.text ?? "") ?? 0
+//        let total = text1 + text2 + text3 + text4 + text5
+//        resultLabel.text = String(total)
 
+//        .mapとreduceを使って書くと
+        let textFields: [UITextField] = [
+            textField1,
+            textField2,
+            textField3,
+            textField4,
+            textField5
+        ]
+        let total = textFields
+            .map { $0.text! } //.map{ $0.text ?? "" }でもできる
+            .map { Int($0)} //.map{ Int($0) ?? 0 }も可能
+            .map { $0 ?? 0 }//何番目かの値がnilだったら0にする。
+            .reduce(0, +)
+        resultLabel.text = String(total)
     }
-    func a() {
-        let text1 = Int(textField1.text ?? "") ?? 0
-        let text2 = Int(textField2.text ?? "") ?? 0
-        let text3 = Int(textField3.text ?? "") ?? 0
-        let text4 = Int(textField4.text ?? "") ?? 0
-        let text5 = Int(textField5.text ?? "") ?? 0
-        resultLabel.text = String(text1 + text2 + text3 + text4 + text5)
-    }
+
+
 }
-
